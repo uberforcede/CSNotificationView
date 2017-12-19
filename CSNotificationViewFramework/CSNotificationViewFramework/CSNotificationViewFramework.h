ae45c6a86804b26de745f81b8da3aa083a87dc2e
Jose Miguel Benedicto Ruiz <jmbene@gmail.com>
2017-12-19 17:07:09 +0100

    Create subproject to add a dynamic framework
    Create subproject to add a dynamic framework

---
+++ CSNotificationViewFramework/CSNotificationViewFramework/CSNotificationViewFramework.h
@@ -0,0 +1,22 @@
+//
+//  CSNotificationViewFramework.h
+//  CSNotificationViewFramework
+//
+//  Created by Jose Miguel Benedicto Ruiz on 19/12/2017.
+//  Copyright © 2017 FinanceApp AG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CSNotificationViewFramework.
+FOUNDATION_EXPORT double CSNotificationViewFrameworkVersionNumber;
+
+//! Project version string for CSNotificationViewFramework.
+FOUNDATION_EXPORT const unsigned char CSNotificationViewFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CSNotificationViewFramework/PublicHeader.h>
+
+#import <CSNotificationView/CSNotificationView.h>
+#import <CSNotificationView/CSNativeBlurView.h>
+#import <CSNotificationView/CSLayerStealingBlurView.h>
+
